34959b102f778d2fc9ac7f536f03dd61d9995ccc
Darnell Andries <dandries@brave.com>
2024-01-19 16:31:33 -0800

    Add Android support the News V2 P3A metrics

--- android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
+++ android/java/org/chromium/chrome/browser/ntp/BraveNewTabPageLayout.java
@@ -540,8 +540,7 @@ public class BraveNewTabPageLayout
                                         }
                                     }
                                     if (mBraveNewsController != null) {
-                                        mBraveNewsController.onSessionCardViewsCountChanged(
-                                                (short) mNewsSessionCardViews, newCardViews);
+                                        mBraveNewsController.onNewCardsViewed(newCardViews);
                                     }
                                     mPrevVisibleNewsCardPosition = lastVisibleItemPosition;
                                 }
